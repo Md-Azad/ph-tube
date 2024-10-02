ca37ea3e9f4da03b7caa682191c5c13811536b4e
Md Azad Hossain <sokalazad@gmail.com>
2024-10-02 22:49:33 +0200

    ph-tube initialized and created responsive navbar

---
+++ js/video.js
@@ -0,0 +1,16 @@
+// fetch, load, and show categories on html
+
+// create loadCategories funciton
+
+const loadCategories = () => {
+  fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+    .then((res) => res.json())
+    .then((data) => displayCategories(data.categories))
+    .catch((err) => console.log(err));
+};
+
+const displayCategories = (data) => {
+  console.log(data);
+};
+
+loadCategories();
